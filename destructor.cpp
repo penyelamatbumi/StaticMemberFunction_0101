28f0b2bd05b7f9010fd04bc367c70279727d237f
penyelamatbumi <inihanip7@gmail.com>
2026-05-05 18:59:12 +0700

    implementasi angka dan delete

--- destructor.cpp
+++ destructor.cpp
@@ -27,3 +27,29 @@ angka::~angka(){ // Destructor
     cout << "Alamat Array Sudah Dilepaskan" << endl;
 }
 
+void angka::cetakData()
+{
+    for (int i = 1; i <= panjang; i++) // loop cetak
+    {
+        cout << i << " = " << arr[i] << endl;
+    }
+}
+
+void angka::isiData()
+{
+    for (int i = 1; i <= panjang; i++) // loop input
+    {
+        cout << i << " = ";
+        cin >> arr[i];
+    }
+    cout << endl;
+}
+
+int main()
+{
+    angka belajarcpp(3);
+    angka *ptrBelajarcpp = new angka(5); // Constructor Dipanggil
+    delete ptrBelajarcpp;                // Destructor Dipanggil
+
+    return 0;
+}
