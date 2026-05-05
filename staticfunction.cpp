4f21cab0a43caa9fd6f459005ba55c0843d040b2
penyelamatbumi <inihanip7@gmail.com>
2026-05-05 19:13:49 +0700

    menambahkan constructor dengan parameter

--- staticfunction.cpp
+++ staticfunction.cpp
@@ -36,3 +36,26 @@ void Mahasiswa::setID() {
     id = ++nim;
 }
 
+void Mahasiswa::printAll() {
+    cout << "ID = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
+
+int main() {
+    // pembuatan object dan pemberian nilai
+    Mahasiswa mhs1("Sri Dadi");
+    Mahasiswa mhs2("Budi Jatiko");
+
+    // memberi nilai awal pada setNim() untuk merubah nilai NIM
+    Mahasiswa::setNim(9);
+    Mahasiswa mhs3("Andi Janu");
+    Mahasiswa mhs4("joko wahono");
+
+    mhs1.printAll();
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
+    
+    return 0;
+}
