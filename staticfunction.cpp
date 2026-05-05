09ba5fd1e18fc659ea14b6f484a54a100a1a511d
penyelamatbumi <inihanip7@gmail.com>
2026-05-05 19:11:26 +0700

    menambahkan static function setnim dan getnim

--- staticfunction.cpp
+++ staticfunction.cpp
@@ -23,39 +23,3 @@ public:
         return nim;
     }
 
-    Mahasiswa(string pnama) : nama(pnama) {
-        setID();
-    }
-};
-
-// inisialisasi static member
-int Mahasiswa::nim = 0;
-
-// pendefinisian prosedur di luar class
-void Mahasiswa::setID() {
-    id = ++nim;
-}
-
-void Mahasiswa::printAll() {
-    cout << "ID = " << id << endl;
-    cout << "Nama = " << nama << endl;
-    cout << endl;
-}
-
-int main() {
-    // pembuatan object dan pemberian nilai
-    Mahasiswa mhs1("Sri Dadi");
-    Mahasiswa mhs2("Budi Jatiko");
-
-    // memberi nilai awal pada setNim() untuk merubah nilai NIM
-    Mahasiswa::setNim(9);
-    Mahasiswa mhs3("Andi Janu");
-    Mahasiswa mhs4("joko wahono");
-
-    mhs1.printAll();
-    mhs2.printAll();
-    mhs3.printAll();
-    mhs4.printAll();
-    
-    return 0;
-}
