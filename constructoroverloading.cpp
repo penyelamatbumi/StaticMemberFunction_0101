6b4de2b02ed6898c4e831838af260971f461f197
penyelamatbumi <inihanip7@gmail.com>
2026-05-05 19:10:20 +0700

    implementasi static member nim

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -33,3 +33,17 @@ mahasiswa :: mahasiswa(int iNim, string iNama){
 void mahasiswa :: cetak(){
     cout << endl << "NIM : " << nim << endl;
     cout << "Nama= : " << nama << endl;
+}
+int main(){
+    mahasiswa mhs1(102030);
+    mahasiswa mhs2("faid");
+    mahasiswa mhs3(010203, "yuratama");
+
+    cout << "Mahasiswa 1:" ; mhs1.cetak();
+
+    cout << " Mahasiswa 2:" ; mhs2.cetak();
+
+    cout << "Mahasiswa 3:" ; mhs3.cetak();
+
+    return 0;
+}
