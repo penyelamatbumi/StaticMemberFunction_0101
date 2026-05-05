bdce2a92e926bdb56591270d7256d80f73bd8a14
penyelamatbumi <inihanip7@gmail.com>
2026-05-05 18:56:02 +0700

    menambahkan method isidata

--- destructor.cpp
+++ destructor.cpp
@@ -19,3 +19,28 @@ angka::angka(int i){ // Constructor
     arr = new int[i];
     isiData();
 }
+
+angka::~angka(){ // Destructor
+    cout << endl;
+    cetakData();
+    delete[] arr;
+    cout << "Alamat Array Sudah Dilepaskan" << endl;
+}
+
+void angka::cetakData()
+{
+    for (int i = 1; i <= panjang; i++) // loop cetak
+    {
+        cout << i << " = " << arr[i] << endl;
+    }
+}
+
+void angka::isiData()
+{
+    for (int i = 1; i <= panjang; i++) // loop input
+    {
+        cout << i << " = ";
+        cin >> arr[i];
+    }
+    cout << endl;
+}
