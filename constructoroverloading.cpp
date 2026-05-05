bc0f9344fd27317adec04c01d058cdffe00efeaa
penyelamatbumi <inihanip7@gmail.com>
2026-05-05 19:09:45 +0700

    menambahkan method cetak data mahasiswa

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -30,3 +30,6 @@ mahasiswa :: mahasiswa(int iNim, string iNama){
     nim = iNim; // definisi NIM dan NAMA
     nama = iNama;
 }
+void mahasiswa :: cetak(){
+    cout << endl << "NIM : " << nim << endl;
+    cout << "Nama= : " << nama << endl;
